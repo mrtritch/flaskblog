7d3f1bcfa785ccd2ac6243212d8742844f1e0e40
traze <ahmdymhmd383@gmail.com>
2024-04-13 02:34:34 +0330

    creat admin and user mod

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from config import Development
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+app.config.from_object(Development)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 @app.route('/')
 def home():
     return 'this is Home page!'
 
 from mod_admin import admin
+from mod_users import users
 
+app.register_blueprint(users)
 app.register_blueprint(admin)
